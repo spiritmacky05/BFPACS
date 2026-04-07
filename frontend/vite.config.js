60e51fb1cb89cdc24ac3842412068bd029e91e78
coelacanth <coelacanth@debian.debian.com>
2026-04-07 22:20:38 +0800

    added some dev thing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,5 +13,11 @@ export default defineConfig({
   },
   server: {
     port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://backend:8080',
+        changeOrigin: true,
+      },
+    },
   },
 })
